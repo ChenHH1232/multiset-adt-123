4e5d8d8faeeaf0724d8c9aa1ada3a7854fce12d1
Quincygithub6 <129473729+Quincygithub6@users.noreply.github.com>
2024-09-16 20:49:33 -0400

    Write init for Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,18 @@
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    private Integer root;
+    private Tree[] sublist;
+
+    public Tree(Integer root, Tree[] sublist) {
+
+        this.root = root;
+        if (sublist == null) {
+            this.sublist = new Tree[0]; // default to empty sublist if null is passed in.
+        }
+        else {
+            this.sublist = sublist;
+        }
+    }
 }
+
